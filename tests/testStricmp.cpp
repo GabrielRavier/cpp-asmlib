6a1dc8080f518393f899586c97bc84341c0f98fb
Gabriel Ravier <gabravier@gmail.com>
2019-05-21 15:36:11 +0200

    Added some tests for stricmp
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- tests/testStricmp.cpp
+++ tests/testStricmp.cpp
@@ -6,7 +6,7 @@
 #include <string_view>
 #include <cassert>
 
-int main()
+void test1()
 {
 	const char *testStr = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890 @`'{}[]()<>";
 	constexpr size_t strSize = 0x100;
@@ -29,4 +29,16 @@ int main()
 	// Make strings different
 	str2[7] -= 2;
 	assert(A_stricmp(str1, str2) > 0);
+}
+
+void test2()
+{
+	assert(A_stricmp("foo", "bar") == -(A_stricmp("bar", "foo")));	// Test result consistency
+	assert(A_stricmp("fOO", "Foo") == 0 && A_stricmp("Foo", "fOO") == 0);	// Test case-insensitively identical strings
+}
+
+int main()
+{
+	test1();
+	test2();
 }

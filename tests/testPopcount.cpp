2f58aac2ec16acf9cdd40f2a9b3d68d1a90aef9f
Gabriel Ravier <gabravier@gmail.com>
2019-05-23 10:23:53 +0200

    Update TODO.txt and add a few things to testPopcount.cpp
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- tests/testPopcount.cpp
+++ tests/testPopcount.cpp
@@ -7,6 +7,14 @@
 uint32_t popcountSSE42(uint32_t x);
 uint32_t popcountGeneric(uint32_t x);
 
+static uint32_t naivePopcount(uint32_t x)
+{
+	uint32_t count = 0;
+	for (; x; ++count)
+		x &= x - 1;
+	return count;
+}
+
 static uint32_t popcountReference(uint32_t x)
 {
 	if (sizeof(x) == sizeof(unsigned int))
@@ -17,6 +25,8 @@ static uint32_t popcountReference(uint32_t x)
 
 	if (sizeof(x) == sizeof(unsigned long long))
 		return __builtin_popcountll(x);
+
+	return naivePopcount(x);
 }
 
 int main()
@@ -25,7 +35,7 @@ int main()
 	if (InstructionSet() >= 9)
 		availableFuncs.push_back(popcountGeneric);
 
-	uint32_t testedVals[] = {1, 0, 54, 23, 64, 87, 0xFFFFFFFF, 0x80000000, 0x23984823, 4921904, 12345, 33};
+	uint32_t testedVals[] = {1, 0, 54, 23, 64, 87, 0xFFFFFFFF, 0x80000000, 0x23984823, 4921904, 12345, 33, 0xF0F0F0F0, 0x55555555, 0x33333333, 0xF0F0F0F};
 
 	for (auto i : testedVals)
 		for (auto func : availableFuncs)
